0a9ef79c1b6fa975dc93d455bc9e0592e8e35dcc
PC-20180826ZCTY\Administrator <243581579@qq.com>
2019-07-22 15:25:34 +0800

    在遍历 connections 时加入判断排除协程

--- WebSocket.php
+++ WebSocket.php
@@ -4,6 +4,7 @@
  */
 
 require "DoRedis.php";
+require "DoPush.php";
 
 class WebSocket {
 
@@ -30,7 +31,7 @@ class WebSocket {
         //  创建一个新的协程,并立即执行; go 相当于 Swoole\Coroutine::create
         go(function (){
             //  下面开启的话会在下方有一个警告 fd[0] is invalid
-            //DoRedis::getRedis()->del('userList');
+            DoRedis::getRedis()->del('userList');
         });
         echo 'start success';
     }
@@ -45,7 +46,7 @@ class WebSocket {
         //  当连接成功的时候将用户信息保存到 redis 的集合中
         DoRedis::getRedis()->sAdd('userList', $request->fd);
         //  向客户端推送数据
-        $this->push($serv);
+        DoPush::getPush()->push($serv);
         echo "server: handshake success with fd{$request->fd}\n";
     }
 
@@ -57,12 +58,8 @@ class WebSocket {
     public function onMessage($serv, $frame)
     {
         echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-        //  遍历所有连接的客户端
-        foreach($serv->connections as $fd)
-        {
-            //  向发送消息的客户端推送数据
-            $serv->push($fd, $this->param($frame->data,$fd,$frame->fd));
-        }
+        //  向所有客户端推送消息
+        DoPush::getPush()->pushMessage($serv, $frame);
     }
 
     /**
@@ -75,37 +72,8 @@ class WebSocket {
         //  当连接断开的时候将用户信息从 redis 的集合中移除
         DoRedis::getRedis()->sRem('userList', $fd);
         //  向客户端推送数据
-        $this->push($serv);
+        DoPush::getPush()->push($serv);
         echo "client {$fd} closed\n";
-    }
-
-    /**
-     * 向当前 demo 客户端推送数据
-     * @param $serv
-     */
-    private function push($serv)
-    {
-        //  遍历所有连接的客户端
-        foreach($serv->connections as $fd)
-        {
-            //  向发送消息的客户端推送数据
-            $serv->push($fd, DoRedis::getRedis()->sCard('userList'));
-        }
-    }
-
-    /**
-     * 拼接当前 demo 所需的数据
-     * @param $data
-     * @param $fd
-     * @param $frame_fd
-     * @return false|string
-     */
-    private function param($data,$fd,$frame_fd)
-    {
-        $data = json_decode($data);
-        $fd == $frame_fd ? $data->status = 0 : $data->status = 1;
-        $data->time = date('Y-m-d H:i:s', time());
-        return json_encode($data);
     }
 }
 
